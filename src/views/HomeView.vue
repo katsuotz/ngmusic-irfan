9205ca067a49ebff4edc2d14661b3070257c26d4
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2022-11-10 19:33:48 +0700

    fixing mobile screen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ music.inputSearch = ''
 </script>
 
 <template>
-  <div class="h-screen w-screen relative bg-gradient-to-b from-primary to-primary-light">
+  <div class="inset-y-0 w-screen relative bg-gradient-to-b from-primary to-primary-light">
     <img class="absolute inset-x-0 mx-auto inset-y-0 my-auto pointer-events-none" src="../assets/logo.png" alt="ngmusic logo">
 
     <form @submit.prevent="() => music.handleMusicRouter()" class="absolute bottom-0 p-5 flex flex-col gap-4 left-0 right-0">
