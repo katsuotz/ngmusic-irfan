709b7c6c95cf9cde37d0d5a9963e5cbe8732156b
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2022-11-10 19:38:32 +0700

    fixing mobile screen

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 </script>
 
 <template>
